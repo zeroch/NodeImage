c10a729495e378880835b95fb647d75d48eca0ff
zeroch <chenze62@gmail.com>
2016-11-19 02:19:12 -0800

    finish the basic function that handle get method, parse query then return two image

---
+++ index.js
@@ -0,0 +1,55 @@
+var http 	=	require('http'),
+	url 	=	require('url'),
+	path 	=	require('path'),
+	fs 		=	require('fs');
+
+var mimeType = {
+	"html": "text/html",
+	"jpeg": "image/jpeg",
+	"png": "image/png"
+}
+
+
+http.createServer(function(request, response){
+	console.log('request ', request.url);
+
+	var headers = request.headers;
+	var method = request.method;
+
+	var parseObject = url.parse(request.url, true);
+	var queryData = parseObject.query;
+
+	response.writeHead(200, {"Content-Type": "text/html"});
+	if (request.method === 'GET' && parseObject.pathname === '/cat') {
+		response.write("Here is valid check \n");
+		if ( queryData.name )
+		{
+			if (queryData.name === "mengmeng")
+				var imageName = "xx.jpg";
+			else
+				var imageName = "yy.jpg";
+
+			response.write(imageName + '\n');
+			fs.readFile(imageName, function(err, content){
+				if (err)  throw err;
+				imagedata = new Buffer(content).toString('base64');
+				response.write('<img src="data:image/jpg;base64,' + imagedata + '" height="640" width="480">');
+				response.end();
+
+			});
+			
+		} else
+		{
+			response.write("no valid name is given.");
+			response.end();
+
+		}
+	} else {
+		response.statusCode = 404;
+		response.write("wrong path");
+		response.end();
+	  }
+
+
+}).listen(8888);
+console.log('server running at http://localhost:8888/');
